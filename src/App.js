7fef10da78b4d3cab7f3aaccb2aae7ccabaa5f3e
justinluu8235 <justinluu8235@gmail.com>
2022-01-13 21:31:38 -0800

    bulma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <div>
+            <Routes>
+              <Route exact path='/' element={<HomePage/>}/>
+            </Routes>
+          </div>
+      </BrowserRouter>
     </div>
   );
 }
