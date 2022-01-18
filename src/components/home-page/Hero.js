9d78e368c877f98608c9fd4be1e15864522064a0
justinluu8235 <justinluu8235@gmail.com>
2022-01-18 14:55:59 -0800

    mobile

--- src/components/home-page/Hero.js
+++ src/components/home-page/Hero.js
@@ -5,54 +5,55 @@ const Hero = (props) => {
     const [heroHeight, setHeroHeight] = useState(0);
 
     useEffect(() => {
+       //get the hero div height
+        let heroDivHeight = document.querySelector('.hero-body').clientHeight;
+        let heroDivWidth = document.querySelector('.hero-body').clientWidth;
+        
+        if(heroDivWidth > 650){
 
-        // //get the hero div height
-        // let heroDivHeight = document.querySelector('.hero-body').clientHeight;
-        // let heroDivWidth = document.querySelector('.hero-body').clientWidth;
-        // console.log("height", heroDivHeight)
+        // Some random colors
+        const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
+        const numBalls = 50;
+        const balls = [];
 
-        // // Some random colors
-        // const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+        for (let i = 0; i < numBalls; i++) {
+            let ball = document.createElement("div");
+            ball.classList.add("ball");
+            ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+            ball.style.left = `${Math.floor(Math.random() * heroDivWidth)}px`;
+            ball.style.top = `${Math.floor(Math.random() * heroDivHeight)}px`;
+            ball.style.transform = `scale(${Math.random()})`;
+            ball.style.width = `${Math.random()}em`;
+            ball.style.height = ball.style.width;
 
-        // const numBalls = 50;
-        // const balls = [];
+            balls.push(ball);
+            document.body.append(ball);
+        }
 
-        // for (let i = 0; i < numBalls; i++) {
-        //     let ball = document.createElement("div");
-        //     ball.classList.add("ball");
-        //     ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-        //     ball.style.left = `${Math.floor(Math.random() * heroDivWidth)}px`;
-        //     ball.style.top = `${Math.floor(Math.random() * heroDivHeight)}px`;
-        //     ball.style.transform = `scale(${Math.random()})`;
-        //     ball.style.width = `${Math.random()}em`;
-        //     ball.style.height = ball.style.width;
+        // Keyframes
+        balls.forEach((el, i, ra) => {
+            let to = {
+                x: Math.random() * (i % 2 === 0 ? -11 : 11),
+                y: Math.random() * 12
+            };
 
-        //     balls.push(ball);
-        //     document.body.append(ball);
-        // }
-
-        // // Keyframes
-        // balls.forEach((el, i, ra) => {
-        //     let to = {
-        //         x: Math.random() * (i % 2 === 0 ? -11 : 11),
-        //         y: Math.random() * 12
-        //     };
-
-        //     let anim = el.animate(
-        //         [
-        //             { transform: "translate(0, 0)" },
-        //             { transform: `translate(${to.x}rem, ${to.y}rem)` }
-        //         ],
-        //         {
-        //             duration: (Math.random() + 1) * 2000, // random duration
-        //             direction: "alternate",
-        //             fill: "both",
-        //             iterations: Infinity,
-        //             easing: "ease-in-out"
-        //         }
-        //     );
-        // });
+            let anim = el.animate(
+                [
+                    { transform: "translate(0, 0)" },
+                    { transform: `translate(${to.x}rem, ${to.y}rem)` }
+                ],
+                {
+                    duration: (Math.random() + 1) * 2000, // random duration
+                    direction: "alternate",
+                    fill: "both",
+                    iterations: Infinity,
+                    easing: "ease-in-out"
+                }
+            );
+        });
+    }
+ 
     })
 
     return (
